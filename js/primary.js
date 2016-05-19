5dcc0badc08baba6db943388d97809bf2a1f1b84
Jared <jmw535@cornell.edu>
2016-05-19 00:12:47 -0400

    Fixed container for main site

--- js/primary.js
+++ js/primary.js
@@ -1,8 +1,21 @@
-$( document).ready(function () {
+$( document ).ready(function () {
+  console.log("test");
 	loadMain();
+});
+
+$('#CAH').click(function(){
+  console.log("test2");
+  loadCards();
 });
 
 function loadMain(){
 	$('#main_page').load('main.html');
 	window.scrollTo(0,0);
 }
+
+function loadCards(){
+	$('#main_page').load('CardsAgainstOCaml.html');
+	window.scrollTo(0,0);
+}
+
+  
