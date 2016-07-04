a420fb57873119f71b787258ac20fc55cf2bb915
Jared <jmw535@cornell.edu>
2016-07-04 09:25:23 -0400

    Dynamic page selection with hasher

--- js/primary.js
+++ js/primary.js
@@ -3,39 +3,22 @@ var aboutCol = "#9a1616";
 var musicCol = "#16499a";
 var devCol = "#128023";
 var homeCol = "#a9a9a9";
+
+var colors = {
+  about: aboutCol,
+  music: musicCol,
+  dev: devCol,
+  main: homeCol
+}
+
+function getColor(str){
+  return colors[str];
+}
+
+
 function handleChanges(newHash, oldhash){
-  switch(newHash){
-    case "":
-      loadMain();
-      break;
-    case "CAH":
-      loadCards();
-      break;
-    case "ClassTeX":
-      loadClassTeX();
-      break;
-    case "Pitbull":
-      loadPitbull();
-      break;
-    case "MTT":
-      loadMTT();
-      break;
-    case "GenerateEDM":
-      loadGenerateEDM();
-      break;
-    case "Resume":
-      loadResume();
-      break;
-    case "Experience":
-      loadExperience();
-      break;
-    case "Arrangements":
-      loadArrangements();
-      break;
-    case "Composition":
-      loadComposition();
-      break;
-  }
+  x = newHash.split(/\//);
+  loadMaster(x);
 }
 
 
@@ -43,82 +26,18 @@ $( document ).ready(function () {
 hasher.changed.add(handleChanges);
 hasher.initialized.add(handleChanges);
 hasher.init();
+hasher.setHash('main/Home');
 });
 
-
-function loadMain(){
-  $('#main_page').load('main.html');
-  changeBorderColor(homeCol);
-  document.title="Home | Jared Wong";
-  window.scrollTo(0,0);
-}
-
-function loadCards(){
-  $('#main_page').load('CardsAgainstOCaml.html');
-  changeBorderColor(devCol);
-  document.title="CAH | Jared Wong";
-  window.scrollTo(0,0);
-}
-
-function loadPitbull(){
-  $('#main_page').load('Pitbull_Generator.html');
-  changeBorderColor(devCol);
-  document.title="Pitbull Generator | Jared Wong"
-  window.scrollTo(0,0);
-}
-
-function loadClassTeX(){
-  $('#main_page').load('ClassTeX.html');
-  changeBorderColor(devCol);
-  document.title = "ClassTeX | Jared Wong"
-  window.scrollTo(0,0);
-}
-
-function loadGenerateEDM(){
-$('#main_page').load('EDMGen.html');
-changeBorderColor(devCol);
-document.title = "EDMGen | Jared Wong"
-  window.scrollTo(0,0);
-}
-
-
-function loadMTT(){
-  $('#main_page').load('MeasuresTempoTimeConverter.html');
-  changeBorderColor(devCol);
-  document.title = "MTT | Jared Wong";
-  window.scrollTo(0,0);
-}
-
-function loadArrangements(){
-  $('#main_page').load('Arrangements.html');
-  changeBorderColor(musicCol);
-  document.title = "Arrangements | Jared Wong"
-  window.scrollTo(0,0);
-}
-
-function loadComposition(){
-  $('#main_page').load('Composition.html');
-  changeBorderColor(musicCol);
-  document.title = "Composition | Jared Wong";
-  window.scrollTo(0,0);
-}
-
-function loadResume(){
-  $('#main_page').load('Resume.html');
-  changeBorderColor(aboutCol);
-  document.title = "Resume | Jared Wong";
-  window.scrollTo(0,0);
-}
-
-function loadExperience(){
-  $('#main_page').load('Experience.html');
-  changeBorderColor(aboutCol);
-  document.title = "Experience | Jared Wong";
+function loadMaster(hashArr){
+  $('#main_page').load(hashArr[1]+'.html');
+  hashArr[0] ? changeBorderColor(getColor(hashArr[0])) : changeBorderColor(homeCol);
+  titleString = hashArr[1] + ' | Jared Wong';
+  document.title= titleString.replace(/_/g, " "); 
   window.scrollTo(0,0);
 }
 
 function changeBorderColor( color){
-  console.log(color);
   $('#main_nav').animate({backgroundColor: color}, 250);
   $('#nav_buttons').animate({backgroundColor: color}, 250);
 }
