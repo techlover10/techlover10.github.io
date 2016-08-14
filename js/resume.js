45801173758d069b438e104fc1ca429b5acd14a3
Jared Wong <techlover10@gmail.com>
2016-08-14 16:18:25 -0700

    Revert to use of jquery for positioning

--- js/resume.js
+++ js/resume.js
@@ -1,16 +1,8 @@
+function fixAlign () {
+  var currWidth = $(window).width();
+  var correctAlign = (currWidth < PHONE_MODE) ? "left" : "right";
+  $( ".secTitle").css("text-align", correctAlign);
+};
+fixAlign();
+$( window ).resize(fixAlign);
 
-$( window ).resize(function () {
-    currentViewModel.windowState($(window).width() < PHONE_MODE ? "phone" : "normal");
-    });
-
-function siteViewModel(){
-  var self = this;
-  self.windowState = ko.observable($(window).width() < PHONE_MODE ? "phone" : "normal");
-  self.resumeAlign = ko.observable("right");
-  self.calculateVal = ko.computed( function () {
-      self.resumeAlign(self.windowState()=="phone" ? "left" : "right");
-      });
-}
-var currentViewModel = new siteViewModel();
-
-ko.applyBindings(currentViewModel);
