29b6dab79c34dd3642ffd17454cc8d232f152bc0
Jared <jmw535@cornell.edu>
2016-12-02 21:06:12 -0500

    indent fixes + css changes

--- js/primary.js
+++ js/primary.js
@@ -11,109 +11,109 @@ var FADE_OUT_DURATION = 200;
 var COLOR_CHANGE_DURATION = 250;
 
 var colors = {
-  about: aboutCol,
-  music: musicCol,
-  dev: devCol,
-  main: homeCol
+    about: aboutCol,
+    music: musicCol,
+    dev: devCol,
+    main: homeCol
 }
 
 function getColor(str){
-  return colors[str];
+    return colors[str];
 }
 
 function doesResourceExist(url){
-  var http = new XMLHttpRequest();
-  http.open('HEAD', url, false);
-  http.send();
-  return http.status!=404;
+    var http = new XMLHttpRequest();
+    http.open('HEAD', url, false);
+    http.send();
+    return http.status!=404;
 }
 
 function handleChanges(newHash, oldhash){
-  crossroads.parse(newHash);
+    crossroads.parse(newHash);
 }
 
 
 $( document ).ready(function () {
-  hasher.init();
-  hasher.changed.add(handleChanges);
-  hasher.initialized.add(handleChanges);
+    hasher.init();
+    hasher.changed.add(handleChanges);
+    hasher.initialized.add(handleChanges);
 });
 
 function finalDisplay(){
-  $('#main_page').show();
-  $('#loadingCircle').velocity("fadeOut", { duration: CIRC_DURATION });
+    $('#main_page').show();
+    $('#loadingCircle').velocity("fadeOut", { duration: CIRC_DURATION });
 }
 
 function redisplay(){
-  $('#main_page').waitForImages(finalDisplay);
+    $('#main_page').waitForImages(finalDisplay);
 }
 
 function loadMaster(hashArr, optionalArgs){
-  $('#main_page').hide();
-  var loadMasterHelper = function () {
-    var resourceName = 'templates/' + hashArr[1]+'.html';
-    $('#main_page').load((doesResourceExist(resourceName) ? resourceName : ERROR_PAGE), redisplay);
-  }
-  $('#loadingCircle').velocity("fadeIn", { duration: CIRC_DURATION, complete: loadMasterHelper});
-  hashArr[0] ? changeBorderColor(getColor(hashArr[0])) : changeBorderColor(homeCol);
-  titleString = hashArr[1] + ' | Jared Wong';
-  document.title= titleString.replace(/_/g, " "); 
-  window.scrollTo(0,0);
+    $('#main_page').hide();
+    var loadMasterHelper = function () {
+        var resourceName = 'templates/' + hashArr[1]+'.html';
+        $('#main_page').load((doesResourceExist(resourceName) ? resourceName : ERROR_PAGE), redisplay);
+    }
+    $('#loadingCircle').velocity("fadeIn", { duration: CIRC_DURATION, complete: loadMasterHelper});
+    hashArr[0] ? changeBorderColor(getColor(hashArr[0])) : changeBorderColor(homeCol);
+    titleString = hashArr[1] + ' | Jared Wong';
+    document.title= titleString.replace(/_/g, " "); 
+    window.scrollTo(0,0);
     if (optionalArgs){
-      loadPage(optionalArgs);
+        loadPage(optionalArgs);
     };
 }
 
 function changeBorderColor( color){
-  $('#main_nav').velocity({backgroundColor: color}, FADE_OUT_DURATION);
-  $('#nav_buttons').velocity({backgroundColor: color}, FADE_OUT_DURATION);
+    $('#main_nav').velocity({backgroundColor: color}, FADE_OUT_DURATION);
+    $('#nav_buttons').velocity({backgroundColor: color}, FADE_OUT_DURATION);
 }
 
 function loadPage(name){
-  //$('#title').velocity("fadeOut", {duration: 500});
-  $('#title').velocity("slideUp", { duration: FADE_SLIDE_DURATION });
-  $('#infoExperience').load('templates/' + name + '.html', calculateHeight);
-  function calculateHeight(){
-    var height = $('#infoExperience').height() + 100;
-    $('#mainExperience').velocity({height: height});
-    $('#infoExperience').velocity("fadeIn", { duration: FADE_SLIDE_DURATION});
-  }
-  $('#buttons').velocity({ left: 20});
-  $('#reset').velocity('fadeIn', {duration: FADE_SLIDE_DURATION});
+    //$('#title').velocity("fadeOut", {duration: 500});
+    $('#title').velocity("slideUp", { duration: FADE_SLIDE_DURATION });
+    $('#infoExperience').load('templates/' + name + '.html', calculateHeight);
+    function calculateHeight(){
+        var height = $('#infoExperience').height() + 100;
+        $('#mainExperience').velocity({height: height});
+        $('#infoExperience').velocity("fadeIn", { duration: FADE_SLIDE_DURATION});
+    }
+    $('#buttons').velocity({ left: 20});
+    $('#reset').velocity('fadeIn', {duration: FADE_SLIDE_DURATION});
 }
 
 function loadUpperDiv(name){
-  //$('#title').velocity("fadeOut", {duration: 500});
-  $('#hamburger').velocity("slideDown", { duration: FADE_SLIDE_DURATION });
-  function calculateHeight(){
-    var height = $('#hamburger').height() + 100;
-    $('#mainPage').velocity({height: height});
-  }
-  $('#buttons').velocity({ left: 20});
-  $('#reset').velocity('fadeIn', {duration: FADE_SLIDE_DURATION});
+    //$('#title').velocity("fadeOut", {duration: 500});
+    $('#hamburger').velocity("slideDown", { duration: FADE_SLIDE_DURATION });
+    function calculateHeight(){
+        var height = $('#hamburger').height() + 100;
+        $('#mainPage').velocity({height: height});
+    }
+    $('#buttons').velocity({ left: 20});
+    $('#reset').velocity('fadeIn', {duration: FADE_SLIDE_DURATION});
 }
 
 function reset(){
-  $('#title').velocity("slideDown", {duration: FADE_SLIDE_DURATION});
-  $('#infoExperience').velocity("fadeOut", {duration: FADE_OUT_DURATION});
-  $('#reset').velocity('fadeOut', {duration: FADE_SLIDE_DURATION});
-  $('#mainExperience').velocity({height: 70});
+    $('#title').velocity("slideDown", {duration: FADE_SLIDE_DURATION});
+    $('#infoExperience').velocity("fadeOut", {duration: FADE_OUT_DURATION});
+    $('#reset').velocity('fadeOut', {duration: FADE_SLIDE_DURATION});
+    $('#mainExperience').velocity({height: 70});
 }
 
 // Crossroads routing
 var devRoute = crossroads.addRoute('/dev/{id}', function (id){
-  loadMaster(['dev', id]);
+    loadMaster(['dev', id]);
 });
 
 var musicRoute = crossroads.addRoute('/music/{id}', function (id){
-  loadMaster(['music', id]);
+    loadMaster(['music', id]);
 });
 
 var aboutRoute = crossroads.addRoute('/about/{id}', function (id){
-  loadMaster(['about', id]);
+    loadMaster(['about', id]);
 });
 
 var emptyRoute = crossroads.addRoute('/', function (){
-  loadMaster(['main', 'Home']);
+    loadMaster(['main', 'Home']);
 });
 
