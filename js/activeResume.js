5459124846cac5138029a13fa744c39af62e64ff
Jared <jmw535@cornell.edu>
2016-12-29 01:22:44 -0800

    fix bug

--- js/activeResume.js
+++ js/activeResume.js
@@ -1,21 +1,33 @@
-var IS_HIDDEN = true;
+var IS_HIDDEN = {
+    cs: true,
+    other: true
+};
+
+function get_is_hidden(id) {
+    return IS_HIDDEN[id];
+}
+
+function toggle_is_hidden(id){
+    IS_HIDDEN[id] = !IS_HIDDEN[id];
+}
+
 function get_hidden (id) {
     return (id=="cs" ? "Show Computer Science Courses ▼" : "Show Other Courses ▼");
 }
+
 function get_shown (id) {
     return (id=="cs" ? "Hide Computer Science Courses ▼" : "Hide Other Courses ▼");
 }
 
 function showHideCourses (id) {
-  if (IS_HIDDEN){
+  if (get_is_hidden(id)){
     var courseList = $("#" + id + "InternalList").height() + 15;
     $( "#" + id + "List").velocity({height: courseList});
     $( "#" + id + "ShowCourses").text(get_shown(id));
-    IS_HIDDEN = false;
   } else {
     $( "#" + id + "List").velocity({height:0});
     $( "#" + id + "ShowCourses").text(get_hidden(id));
-    IS_HIDDEN = true;
   }
+  toggle_is_hidden(id);
 };
 
